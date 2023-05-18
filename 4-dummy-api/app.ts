0ef1c11d529b6b85a8c6c2b34f83e1cbca9651b4
Sergei Ordin <seryogaordin@gmail.com>
2023-05-18 09:39:39 +0300

    .

--- 4-dummy-api/app.ts
+++ 4-dummy-api/app.ts
@@ -1,78 +1,87 @@
 const URI: string = 'https://dummyjson.com/users';
 
-interface User {
-    users: {
-        id: number;
-        firstName: string;
-        lastName: string;
-        maidenName: string;
-        age: number;
-        gender: string;
-        email: string;
-        phone: string;
-        username: string;
-        password: string;
-        birthDate: string;
-        image: string;
-        bloodGroup: string;
-        height: number;
-        weight: number;
-        eyeColor: string;
-        hair: {
-            color: string;
-            type: string;
-        };
-        domain: string;
-        ip: string;
-        address: {
-            address: string;
-            city: string;
-            coordinates: {
-                lat: number;
-                lng: number;
-            };
-            postalCode: string;
-            state: string;
-        };
-        macAddress: string;
-        university: string;
-        bank: {
-            cardExpire: string;
-            cardNumber: string;
-            cardType: string;
-            currency: string;
-            iban: string;
-        };
-        company: {
-            address: {
-                address: string;
-                city?: string;
-                coordinates: {
-                    lat: number;
-                    lng: number;
-                };
-                postalCode: string;
-                state: string;
-            };
-            department: string;
-            name: string;
-            title: string;
-        };
-        ein: string;
-        ssn: string;
-        userAgent: string;
+interface IHair {
+    color: string;
+    type: string;
+}
+
+interface ICoordinates {
+    lat: number;
+    lng: number;
+}
+
+interface IAddress {
+    address: string;
+    city: string;
+    coordinates: ICoordinates;
+    postalCode: string;
+    state: string;
+}
+
+interface IBank {
+    cardExpire: string;
+    cardNumber: string;
+    cardType: string;
+    currency: string;
+    iban: string;
+}
+
+interface ICompany {
+    address: {
+        address: string;
+        city?: string;
+        coordinates: ICoordinates;
+        postalCode: string;
+        state: string;
     };
+    department: string;
+    name: string;
+    title: string;
+}
+
+interface IUsers {
+    id: number;
+    firstName: string;
+    lastName: string;
+    maidenName: string;
+    age: number;
+    gender: string;
+    email: string;
+    phone: string;
+    username: string;
+    password: string;
+    birthDate: string;
+    image: string;
+    bloodGroup: string;
+    height: number;
+    weight: number;
+    eyeColor: string;
+    hair: IHair;
+    domain: string;
+    ip: string;
+    address: IAddress;
+    macAddress: string;
+    university: string;
+    bank: IBank;
+    company: ICompany;
+    ein: string;
+    ssn: string;
+    userAgent: string;
+}
+
+interface IUser {
+    users: IUsers;
     total: number;
     skip: number;
     limit: number;
 }
 
-async function getUsers(url: string) {
+async function getUsers(url: string): Promise<void | IUser> {
     try {
         const data = await fetch(url)
             .then((res) => res.json())
             .then(console.log);
-        return data as unknown as User;
+        return data;
     } catch (error) {
         console.log(error as Error);
     }
