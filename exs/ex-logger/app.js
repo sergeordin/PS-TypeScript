ed82ff48c66612ea459d1b715d3831b7d55a5ea4
Sergei Ordin <seryogaordin@gmail.com>
2023-05-20 11:53:16 +0300

    ex-logger

---
+++ exs/ex-logger/app.js
@@ -0,0 +1,17 @@
+"use strict";
+class Logger {
+    printDate(date) {
+        this.log(date.toString());
+    }
+}
+class MyLogger extends Logger {
+    log(message) {
+        console.log(message);
+    }
+    logWithDate(message) {
+        this.printDate(new Date());
+        this.log(message);
+    }
+}
+const logger = new MyLogger();
+logger.logWithDate('My message');
