9a8180489b108abbdfda6737337dd1fa7df1f771
Sergei Ordin <seryogaordin@gmail.com>
2023-05-24 20:37:24 +0300

    working variant

--- 5-map/app.js
+++ 5-map/app.js
@@ -1,16 +1,36 @@
 "use strict";
 class MyMap {
-    id;
-    title;
-    price;
-    constructor(id, title, price) {
-        this.id = id;
-        this.title = title;
-        this.price = price;
+    buckets = [];
+    hash(id) {
+        const r = Math.floor(Math.random() * 10 + 1);
+        return id * r;
     }
-    buckets = [{}];
-    set() { }
-    delete() { }
-    get() { }
-    clear() { }
+    set(id, title) {
+        this.buckets.push({
+            id,
+            hash: this.hash(id),
+            title,
+        });
+    }
+    get(id) {
+        return this.buckets.filter((i) => i.id === id);
+    }
+    delete(id) {
+        this.buckets = this.buckets.filter((i) => i.id !== id);
+        return this.buckets;
+    }
+    clear() {
+        this.buckets = [];
+    }
 }
+const m = new MyMap();
+for (let i = 1; i <= 10; i++) {
+    m.set(i, `test${i}`);
+}
+console.log('BUCKETS ', m.buckets);
+console.log('GET ', m.get(2));
+console.log('AFTER GET ', m.buckets);
+m.delete(1);
+console.log('AFTER DELETE ', m.buckets);
+m.clear();
+console.log('AFTER CLEAR ', m.buckets);
--- 5-map/app.ts
+++ 5-map/app.ts
@@ -1,13 +1,46 @@
-class MyMap {
-    constructor(
-        public id: number,
-        public title: string,
-        public price: number
-    ) {}
-    buckets: {}[] = [{}];
-
-    set() {}
-    delete() {}
-    get() {}
-    clear() {}
+interface Buckets {
+    id: number;
+    hash: number;
+    title: string[] | string;
 }
+
+class MyMap {
+    buckets: Buckets[] = [];
+
+    hash(id: number): number {
+        const r: number = Math.floor(Math.random() * 10 + 1);
+        return id * r;
+    }
+
+    set(id: number, title: string) {
+        this.buckets.push({
+            id,
+            hash: this.hash(id),
+            title,
+        });
+    }
+
+    get(id: number) {
+        return this.buckets.filter((i) => i.id === id);
+    }
+    delete(id: number) {
+        this.buckets = this.buckets.filter((i) => i.id !== id);
+        return this.buckets;
+    }
+    clear() {
+        this.buckets = [];
+    }
+}
+
+const m = new MyMap();
+for (let i = 1; i <= 10; i++) {
+    m.set(i, `test${i}`);
+}
+
+console.log('BUCKETS ', m.buckets);
+console.log('GET ', m.get(2));
+console.log('AFTER GET ', m.buckets);
+m.delete(1);
+console.log('AFTER DELETE ', m.buckets);
+m.clear();
+console.log('AFTER CLEAR ', m.buckets);
